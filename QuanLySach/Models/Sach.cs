5f283a1c3d20080f3ae9fbd4f66be3478977de00
Huỳnh Ann <khanggers@gmail.com>
2020-06-25 04:29:06 +0700

    Add project files.

---
+++ QuanLySach/Models/Sach.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLySach.Models
+{
+    public class Sach
+    {
+        public int Id { set; get; }
+        public string Tilte { set; get; }
+        public string  Content { get; set; }
+        public string AuthorName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
